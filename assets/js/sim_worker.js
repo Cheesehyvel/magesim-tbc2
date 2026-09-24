0c33e3df7dadf624974940fc570ff99af64a1296
Cheesehyvel <eric.hoglander@gmail.com>
2026-09-24 17:59:54 +0200

    Final path fix

--- assets/js/sim_worker.js
+++ assets/js/sim_worker.js
@@ -15,7 +15,7 @@ onmessage = (event) => {
     };
 
     if (data.type == "start") {
-        const wasm = fetch("../../magesim.wasm", {cache: "no-store"})
+        const wasm = fetch(import.meta.env.BASE_URL+"magesim.wasm", {cache: "no-store"})
         .then(r => r.arrayBuffer())
         .then(binary => MageSim({wasmBinary: binary}))
         .then(m => {
